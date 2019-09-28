3a600920885ed61ff5071e43301079a8d1b94ff1
Marta Zabój <owsiana.marta@gmail.com>
2019-09-28 14:57:49 +0200

    add entity manager

---
+++ persistence/src/main/java/repository/GenericDao.java
@@ -0,0 +1,48 @@
+package repository;
+
+import java.lang.reflect.ParameterizedType;
+
+import javax.persistence.EntityManager;
+import javax.persistence.EntityTransaction;
+
+public abstract class GenericDao<T, K> {
+
+    protected final EntityManager em;
+    protected final Class<T> entityClass;
+
+    @SuppressWarnings("unchecked")
+    protected GenericDao() {
+        ParameterizedType genericSuperclass = (ParameterizedType) getClass().getGenericSuperclass();
+        this.entityClass = (Class<T>) genericSuperclass.getActualTypeArguments()[0];
+        this.em = EntityManagerUtil.;
+    }
+
+    public void create(T entity) {
+        EntityTransaction transaction = em.getTransaction();
+        transaction.begin();
+        em.persist(entity);
+        transaction.commit();
+    }
+
+    public T read(K id) {
+        return em.find(entityClass, id);
+    }
+
+    public T update(T entity) {
+        EntityTransaction transaction = em.getTransaction();
+        transaction.begin();
+        T merge = em.merge(entity);
+        transaction.commit();
+        return merge;
+    }
+
+    public void delete(K id) {
+        EntityTransaction transaction = em.getTransaction();
+        transaction.begin();
+        T read = read(id);
+        em.remove(read);
+        transaction.commit();
+    }
+}
+
+
